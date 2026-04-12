ea21768fb014689e3575ea79437bf2a93b882c00
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-12 15:56:11 +0200

    x

--- components/sparkles-fullpage-demo.tsx
+++ components/sparkles-fullpage-demo.tsx
@@ -5,7 +5,7 @@ import { SparklesCore } from "@/components/ui/sparkles";
 
 export default function Page() {
   return (
-    <div className="h-[40rem] relative w-full bg-black flex flex-col object-cover h-48 w-full  overflow-hidden rounded-md">
+    <div className="h-[40rem] relative w-full bg-black flex flex-col  h-48 w-full  overflow-hidden rounded-md">
       <div className="w-full absolute inset-0 h-screen">
         <SparklesCore
           id="tsparticlesfullpage"
